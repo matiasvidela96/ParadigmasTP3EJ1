02b9b69cfce3457d5650fca16c61b37db14682c8
Augusto Ana <augustora95@gmail.com>
2022-09-30 21:20:49 -0300

    package asignaturas

--- src/Asignaturas/Curso.java
+++ src/Asignaturas/Curso.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Asignaturas;
 
-/**
- *
- * @author mativ
- */
+import java.util.ArrayList;
+
 public class Curso {
+    private ArrayList <Inscripcion> delAlumno;
+    
+    private void inscripto(){
+        
+    }
     
 }
--- src/Asignaturas/Inscripcion.java
+++ src/Asignaturas/Inscripcion.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Asignaturas;
+import Miembros.*;
 
-/**
- *
- * @author mativ
- */
+
 public class Inscripcion {
-    
+    private Estudiante alumno;
+    private Curso paraEl;
 }
