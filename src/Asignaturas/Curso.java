f1272132aae2ba87a2f656712c5f2e205c6799d8
matiasvidela96 <matividela@yahoo.com.ar>
2022-10-01 17:59:28 -0300

    metodo inscriptos

--- src/Asignaturas/Curso.java
+++ src/Asignaturas/Curso.java
@@ -1,12 +1,41 @@
 package Asignaturas;
+import Miembros.*;
 
 import java.util.ArrayList;
 
 public class Curso {
+    protected String titulo;
     private ArrayList <Inscripcion> delAlumno;
+
+    public Curso(String titulo) {
+        this.titulo = titulo;
+    }
+    
     
     private void inscripto(){
-        
+        System.out.println("Curso: "+ titulo);
+        System.out.println("Cantidad de inscriptos "+ delAlumno.size());
+        for (int i = 0; i < delAlumno.size(); i++) {
+            System.out.println(delAlumno.get(i).getAlumno().getNombre());
+        }
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public ArrayList<Inscripcion> getDelAlumno() {
+        return delAlumno;
+    }
+
+    public void setDelAlumno(Inscripcion i) {
+        delAlumno.add(i);
+    }
+    
+    
     
 }
--- src/Asignaturas/Inscripcion.java
+++ src/Asignaturas/Inscripcion.java
@@ -9,7 +9,26 @@ public class Inscripcion {
     public Inscripcion(Estudiante alumno, Curso paraEl) {
         this.alumno = alumno;
         this.paraEl = paraEl;
+        paraEl.setDelAlumno(this);
     }
+
+    public Estudiante getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Estudiante alumno) {
+        this.alumno = alumno;
+    }
+
+    public Curso getParaEl() {
+        return paraEl;
+    }
+
+    public void setParaEl(Curso paraEl) {
+        this.paraEl = paraEl;
+    }
+    
+    
     
     
 }
